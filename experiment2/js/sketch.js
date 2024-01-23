de4f424d276db12b1c41f7608dc3c5f86d54d68d
GreebleCultist <evanlake583@gmail.com>
2024-01-22 19:06:48 -0800

    Fixed bugs, integrated exp
    fixed some problems with rotation, added stroke weight controls, and integrated the experiment into the main experiment 2 page.

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -2,7 +2,7 @@ let SizeX = 1200;
 let SizeY = 600;
 let SizeH = 0;
 function setup() {
-  /*
+  
   canvasContainer = $("#canvas-container");
   let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
   canvas.parent("canvas-container");
@@ -10,15 +10,14 @@ function setup() {
     console.log("Resizing...");
     resizeCanvas(canvasContainer.width(), canvasContainer.height());
   });
-  */
-  //SizeX = displayWidth;
-  //SizeY = displayHeight;
+  
+  SizeX = canvasContainer.width();
+  SizeY = canvasContainer.height();
   SizeH = sqrt((SizeX * SizeX) + (SizeY *  SizeY));
-  createCanvas(SizeX, SizeY);
+  //createCanvas(SizeX, SizeY);
   angleMode(DEGREES);
   //frameRate(30);
   noFill()
-  strokeWeight(1.5);
 }
 
 function draw() {
@@ -26,10 +25,11 @@ function draw() {
   getInput();
   drawBackGrid();
   drawForeGrid();
+  print (forePeriod + " : " + foreHyp);
 }
 
-let forePeriod = 20;
-let foreCurveLen = 40;
+let forePeriod = 30;
+let foreCurveLen = 120;
 let foreShiftX = 0;
 let foreShiftY = 0;
 let foreMoveX = 0;
@@ -38,33 +38,39 @@ let foreCurveX = 0;
 let foreCurveY = 0;
 let foreAngle = 0;
 let foreGridType = 0;
+let foreWeight = 3;
 function drawForeGrid() {
-  push();
-  if (foreGridType == 0) {
-    
   
+  strokeWeight(foreWeight);
+  if (foreGridType == 0) {
+    push();
+    translate(foreShiftX,foreShiftY);
     translate(SizeX/2,SizeY/2)
     rotate(foreAngle);
-    
-    translate(foreShiftX,foreShiftY);
     makeSquareGrid(SizeH, forePeriod, SizeX/2, SizeY/2);
-    translate(-SizeX/2,-SizeY/2)
+    translate(-SizeX/2,-SizeY/2);
+    pop();
+    
   } else if (foreGridType == 1) {
-    translate(foreMoveX,foreMoveY)
+    push();
+    translate(foreMoveX,foreMoveY);
     makeCircularGrid(SizeH, forePeriod, SizeX/2, SizeY/2);
+    pop();
   } else if (foreGridType == 2) {
+    push();
     translate(SizeX/2,SizeY/2)
     rotate(foreAngle);
     translate(foreCurveX,foreCurveY);
     makeWaveyGrid(SizeH, forePeriod,foreCurveLen, SizeX/2, SizeY/2)
-    translate(-SizeX/2,-SizeY/2)
+    translate(-SizeX/2,-SizeY/2);
+    pop();
   }
-  pop();
+  
 }
 
 let backGridType = 0;
-let backPeriod = 20;
-let backCurveLen = 40;
+let backPeriod = 30;
+let backCurveLen = 60;
 function drawBackGrid() {
   push();
   if (backGridType == 0) {
@@ -113,46 +119,79 @@ function makeWaveyGrid(gSize, period, curveLen, x, y) {
   }
 }
 
+// This code sourced from :https://www.fwait.com/how-to-disable-scrolling-with-arrow-keys-in-javascript/
+document.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "ArrowLeft":
+    case "ArrowRight":
+    case "ArrowUp":
+    case "ArrowDown":
+      e.preventDefault();
+      break;
+  }
+});
+
+let angRad = foreAngle * Math.PI/180;
+let foreHyp = Math.sqrt(forePeriod*forePeriod * 2);
 function getInput() {
-  
   //foregrid shifting
+  
+  
   if (keyIsDown(RIGHT_ARROW)) {
-    if (foreGridType == 0) foreShiftX = (foreShiftX + 1) % forePeriod;
-    if (foreGridType == 1) foreMoveX = (foreMoveX + 1) % SizeX;
-    if (foreGridType == 2) foreCurveX = (foreCurveX + 1) % (foreCurveLen*2);
+    if (foreGridType == 0) {
+      foreShiftX = (foreShiftX + 1) % (6*(lerp(forePeriod,foreHyp,(Math.abs(foreAngle)/45))));
+      //print(((lerp(forePeriod,foreHyp,(Math.abs(foreAngle)/45)))));
+      //print((Math.abs(forePeriod*Math.cos(angRad)) + Math.abs(forePeriod*Math.sin(angRad))));
+    } 
+    else if (foreGridType == 1) foreMoveX = (foreMoveX + 2) % SizeX;
+    else if (foreGridType == 2) {
+      foreCurveX = (foreCurveX + 2) % (foreCurveLen*2);
+    }
   } 
   if (keyIsDown(LEFT_ARROW)) {
-    if (foreGridType == 0) foreShiftX = (foreShiftX - 1) % forePeriod;
-    if (foreGridType == 1) foreMoveX = (foreMoveX - 1) % SizeX;
-    if (foreGridType == 2) foreCurveX = (foreCurveX - 1) % (foreCurveLen*2);
+    if (foreGridType == 0)  {
+      foreShiftX = (foreShiftX - 1) % (6*(lerp(forePeriod,foreHyp,(Math.abs(foreAngle)/45))));
+    }
+    if (foreGridType == 1) foreMoveX = (foreMoveX - 2) % SizeX;
+    if (foreGridType == 2) foreCurveX = (foreCurveX - 2) % (foreCurveLen*2);
   }
   if (keyIsDown(UP_ARROW)) {
-    if (foreGridType == 0) foreShiftY = (foreShiftY - 1) % forePeriod;
-    if (foreGridType == 1) foreMoveY = (foreMoveY - 1) % SizeY;
-    if (foreGridType == 2) foreCurveY = (foreCurveY - 1) % (forePeriod);
+    if (foreGridType == 0) {
+      foreShiftY = (foreShiftY - 1) % (6*(lerp(forePeriod,foreHyp,(Math.abs(foreAngle)/45))));
+    }
+    if (foreGridType == 1) foreMoveY = (foreMoveY - 2) % SizeY;
+    if (foreGridType == 2) foreCurveY = (foreCurveY - 2) % (forePeriod);
   } 
   if (keyIsDown(DOWN_ARROW)) {
-    if (foreGridType == 0) foreShiftY = (foreShiftY + 1) % forePeriod;
-    if (foreGridType == 1) foreMoveY = (foreMoveY + 1) % SizeY;
-    if (foreGridType == 2) foreCurveY = (foreCurveY + 1) % (forePeriod);
+    if (foreGridType == 0) {
+      foreShiftY = (foreShiftY + 1) % (6*(lerp(forePeriod,foreHyp,(Math.abs(foreAngle)/45))));
+    }
+    if (foreGridType == 1) foreMoveY = (foreMoveY + 2) % SizeY;
+    if (foreGridType == 2) foreCurveY = (foreCurveY + 2) % (forePeriod);
   }
   
   // foregrid period scaling
   if (keyIsDown(81)) { // key 'q'
     if (forePeriod < SizeX) forePeriod += 1;
     if (foreCurveLen < SizeX) foreCurveLen += 1;
+    foreHyp = Math.sqrt(forePeriod*forePeriod * 2);
   }
   if (keyIsDown(65)) { // key 'a'
     if (forePeriod > 5) forePeriod -= 1;
     if (foreCurveLen > 10) foreCurveLen -= 1;
+    foreHyp = Math.sqrt(forePeriod*forePeriod * 2);
   }
   
+  
+
   // foregrid rotation
   if (keyIsDown(82)) { // key 'r'
     foreAngle = (foreAngle + 1) % 360;
+    angRad = foreAngle * Math.PI/180;
   }
   if (keyIsDown(70)) { // key 'f'
     foreAngle = (foreAngle - 1) % 360;
+    angRad = foreAngle * Math.PI/180;
   }
   
   
@@ -168,6 +207,16 @@ function keyPressed() {
     }
     if (keyCode ==  83) { // key 's'
       backGridType = (backGridType + 1) % 3;
+    }
+
+    // foregrid thickness
+    if (keyIsDown(69)) { // key 'e'
+      if (foreWeight <= 6) foreWeight = foreWeight + 1;
+      else foreWeight = 3
+    }
+    if (keyIsDown(68)) { // key 'd'
+      if (foreWeight >= 3) foreWeight = (foreWeight - 1);
+      else foreWeight = 6;
     }
   
   // foreColor

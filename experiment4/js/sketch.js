c010f21a0ff34456603441ee54b1b78a21d2f19d
GreebleCultist <evanlake583@gmail.com>
2024-02-05 19:47:39 -0800

    cleaned up code, added to doc
    removed some comments/un-needed code and some added some basic documentation outline.

--- experiment4/js/sketch.js
+++ experiment4/js/sketch.js
@@ -1,14 +1,9 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// sketch.js - This file takes in an image and sorts the image's pixels by hue and lightness
+// Author: Evan Lake
+// Date: 2/5/2024
 
 // Here is how you might set up an OOP p5.js project
 // Note that p5.js looks for a file called sketch.js
-
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
 
 // Globals
 let myInstance;
@@ -20,11 +15,6 @@ var imgSrcFileName = "MacEssence.jpg";
 var imgSrc;            // source image  
 var imgSrcPixels = []; // packed ARGB of source image
 var imgPixels = [];    // packed ARGB of sorted image
- 
-// threshold values to determine sorting start and end pixels
-var blackValue;
-var brightnessValue;
-var whiteValue;
  
 var row = 0;
 var column = 0;
@@ -87,8 +77,7 @@ function setup() {
  
  
 
- function draw() 
- {
+function draw() {
     if (!startSort) return;
     hueSort();
 
@@ -103,19 +92,18 @@ function setup() {
     }
  
      updatePixels();
- }
+}
  
  
- function mouseClicked() 
- {
+function mouseClicked() {
     startSort = true;
     osc = new p5.Oscillator();
     osc.setType("sine");
     osc.start();
- }
+}
  
- var sortArea = 100;
- function hueSort() {
+var sortArea = 100;
+function hueSort() {
     // select random point in image
     var posX = round(random(imgSrc.width-sortArea));
     var posY = round(random(imgSrc.height-sortArea));
@@ -124,19 +112,13 @@ function setup() {
     //get array array of effected area
     var iRow;
     var pixelArr = [];
-    var highHue = 0;
-    var lowHue = 0;
-    var highLight = 0;
-    var lowLight = 0;
 
     for (var i = 0; i < sortArea * sortArea; i += sortArea) {
         var inUnsorted = [];
         iRow = (posY + (i/sortArea)) * imgSrc.width;
         for (var j = 0; j < sortArea; j++) {
             var pixRGB = imgPixels[iRow+posX+j];
-            //if (i==0 && j==0) print("RGB Before: " + (pixRGB >> 16 & 255) + " - " + (pixRGB >> 8 & 255) + " - " + (pixRGB & 255));
             pixelArr[i+j] = rgbToHsl((pixRGB >> 16 & 255), (pixRGB >> 8 & 255), (pixRGB & 255));
-            //print("RGBA: " + (imgPixels[iRow+posX+j] >> 16 & 255) + ", " + (imgPixels[iRow+posX+j] >> 8 & 255) + ", " + (imgPixels[iRow+posX+j] & 255) + ", ");
         }
     }
 
@@ -182,20 +164,20 @@ function setup() {
             imgPixels[iRow+posX+j] = pixRGB;
         }
     }
- }
+}
 
- // RGB to HSL conversion taken from https://gist.github.com/mjackson/5311256
- /**
- * Converts an RGB color value to HSL. Conversion formula
- * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
- * Assumes r, g, and b are contained in the set [0, 255] and
- * returns h, s, and l in the set [0, 1].
- *
- * @param   Number  r       The red color value
- * @param   Number  g       The green color value
- * @param   Number  b       The blue color value
- * @return  Array           The HSL representation
- */
+// RGB to HSL conversion taken from https://gist.github.com/mjackson/5311256
+/**
+* Converts an RGB color value to HSL. Conversion formula
+* adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+* Assumes r, g, and b are contained in the set [0, 255] and
+* returns h, s, and l in the set [0, 1].
+*
+* @param   Number  r       The red color value
+* @param   Number  g       The green color value
+* @param   Number  b       The blue color value
+* @return  Array           The HSL representation
+*/
 function rgbToHsl(r, g, b) {
     r /= 255, g /= 255, b /= 255;
   
@@ -203,56 +185,56 @@ function rgbToHsl(r, g, b) {
     var h, s, l = (max + min) / 2;
   
     if (max == min) {
-      h = s = 0; // achromatic
+        h = s = 0; // achromatic
     } else {
-      var d = max - min;
-      s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-  
-      switch (max) {
-        case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-        case g: h = (b - r) / d + 2; break;
-        case b: h = (r - g) / d + 4; break;
-      }
-  
-      h /= 6;
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+    
+        switch (max) {
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+    
+        h /= 6;
     }
   
     return [ h, s, l ];
-  }
+}
   
-  /**
-   * Converts an HSL color value to RGB. Conversion formula
-   * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
-   * Assumes h, s, and l are contained in the set [0, 1] and
-   * returns r, g, and b in the set [0, 255].
-   *
-   * @param   Number  h       The hue
-   * @param   Number  s       The saturation
-   * @param   Number  l       The lightness
-   * @return  Array           The RGB representation
-   */
-  function hslToRgb(h, s, l) {
+/**
+* Converts an HSL color value to RGB. Conversion formula
+* adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+* Assumes h, s, and l are contained in the set [0, 1] and
+* returns r, g, and b in the set [0, 255].
+*
+* @param   Number  h       The hue
+* @param   Number  s       The saturation
+* @param   Number  l       The lightness
+* @return  Array           The RGB representation
+*/
+function hslToRgb(h, s, l) {
     var r, g, b;
   
     if (s == 0) {
       r = g = b = l; // achromatic
     } else {
-      function hue2rgb(p, q, t) {
-        if (t < 0) t += 1;
-        if (t > 1) t -= 1;
-        if (t < 1/6) return p + (q - p) * 6 * t;
-        if (t < 1/2) return q;
-        if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-        return p;
-      }
+        function hue2rgb(p, q, t) {
+            if (t < 0) t += 1;
+            if (t > 1) t -= 1;
+            if (t < 1/6) return p + (q - p) * 6 * t;
+            if (t < 1/2) return q;
+            if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+            return p;
+        }
   
-      var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-      var p = 2 * l - q;
+        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        var p = 2 * l - q;
   
-      r = hue2rgb(p, q, h + 1/3);
-      g = hue2rgb(p, q, h);
-      b = hue2rgb(p, q, h - 1/3);
+        r = hue2rgb(p, q, h + 1/3);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1/3);
     }
   
     return [ r * 255, g * 255, b * 255 ];
-  }
+}

7b9b51a51164ee3f5f4a457fa91a7b2447da3b1d
GreebleCultist <evanlake583@gmail.com>
2024-02-07 18:13:03 -0800

    got an object moving around
    made my own class object called starParticle, I want to try and make a bunch of extra stuff fly around

--- experiment5/js/sketch.js
+++ experiment5/js/sketch.js
@@ -12,43 +12,67 @@ class solarSystem{
 		this.img=img
 	}
 	show(){
-	push();
-	normalMaterial();
-    rotateY(this.ry);
-	translate(this.x,this.y,this.z);
-	texture(this.img)
-	rotateY(this.ry2);
-	sphere(this.r);
-	pop();
+        push();
+        normalMaterial();
+        rotateY(this.ry);
+        translate(this.x,this.y,this.z);
+        texture(this.img);
+        rotateY(this.ry2);
+        sphere(this.r);
+        pop();
 	}
 	rotateLeft(){
-		this.ry2+=2
+		this.ry2+=2;
 	}
 	orbitMercury(){
-		this.ry-=15
-	}	
-	
+		this.ry-=15;
+	}
 	orbitVenus(){
-		this.ry-=13
+		this.ry-=13;
 	}
 	orbitEarth(){
-		this.ry-=10
+		this.ry-=10;
 	}
 	orbitMars(){
-		this.ry-=8
+		this.ry-=8;
 	}
 	orbitJupiter(){
-		this.ry-=5
+		this.ry-=5;
 	}
 	orbitSaturn(){
-		this.ry-=3
+		this.ry-=3;
 	}
 	orbitUranus(){
-		this.ry-=2
+		this.ry-=2;
 	}
 	orbitNeptune(){
-		this.ry-=1
+		this.ry-=1;
 	}
+}
+
+class starParticle{
+    constructor(x,y,z, speed, r, img){
+		this.x=x
+		this.y=y
+		this.z=z
+        this.speed = speed;
+        this.r = r;
+		this.img=img
+	}
+	show(){
+        push();
+        normalMaterial();
+        translate(this.x,this.y,this.z);
+        texture(this.img);
+        sphere(this.r);
+        pop();
+	}
+	moveUp() {
+        this.y -= this.speed;
+    }
+    moveDown() {
+        this.y += this.speed;
+    }
 }
 // Here is how you might set up an OOP p5.js project
 // Note that p5.js looks for a file called sketch.js
@@ -57,17 +81,18 @@ let myInstance;
 let canvasContainer;
 let sunImage;
 function preload() {
-    sunImage = loadImage('./macEssence.jpg')
-    mercuryImage = loadImage("./macEssence.jpg")
-    venusImage = loadImage("./macEssence.jpg")
-    earthImage = loadImage("./macEssence.jpg")
-    marsImage =loadImage("./macEssence.jpg")
-    jupiterImage =loadImage("./macEssence.jpg")
-    suturnImage = loadImage("./macEssence.jpg")
-    uranusImage = loadImage("./macEssence.jpg")
-    neptuneImage =loadImage("./macEssence.jpg")
-    spaceImage = loadImage("./macEssence.jpg")
-    angleMode(DEGREES)
+    macImage = loadImage('./macEssence.jpg');
+    sunImage = macImage;
+    mercuryImage = macImage;
+    venusImage = macImage;
+    earthImage = macImage;
+    marsImage = macImage;
+    jupiterImage = macImage;
+    suturnImage = macImage;
+    uranusImage = macImage;
+    neptuneImage = macImage;
+    spaceImage = macImage;
+    angleMode(DEGREES);
 	
 }
 
@@ -84,23 +109,33 @@ function setup() {
     });
 
     background(100);
-	sun= new solarSystem(0,0,0,0,0,100,sunImage)
-	mercury = new solarSystem(140,0,0,0,0,20,mercuryImage)
-	venus = new solarSystem(300,0,0,0,0,60,venusImage)
-	earth = new solarSystem(450,0,0,0,0,70,earthImage)
-	mars = new solarSystem(650,0,0,0,0,50,marsImage)
-	jupiter = new solarSystem(850,0,0,0,0,90,jupiterImage)
-	saturn = new solarSystem(1050,0,0,0,0,80,suturnImage)
-	uranus = new solarSystem(1350,0,0,0,0,60,uranusImage)
-	neptune = new solarSystem(1650,0,0,0,0,60,neptuneImage)
-	space =  new solarSystem(0,0,0,0,0,2000,spaceImage)
+    macComet = new starParticle(0,0,0,2,30,macImage);
+	sun= new solarSystem(0,0,0,0,0,100,sunImage);
+	mercury = new solarSystem(140,0,0,0,0,20,mercuryImage);
+	venus = new solarSystem(300,0,0,0,0,60,venusImage);
+	earth = new solarSystem(450,0,0,0,0,70,earthImage);
+	mars = new solarSystem(650,0,0,0,0,50,marsImage);
+	jupiter = new solarSystem(850,0,0,0,0,90,jupiterImage);
+	saturn = new solarSystem(1050,0,0,0,0,80,suturnImage);
+	uranus = new solarSystem(1350,0,0,0,0,60,uranusImage);
+	neptune = new solarSystem(1650,0,0,0,0,60,neptuneImage);
+	space =  new solarSystem(0,0,0,0,0,2000,spaceImage);
 }
     
-
+var testGo = false;
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
 	orbitControl(); 
 	background(100);
+    if (macComet.y >= 200) testGo = true;
+    else if (macComet.y < -200) testGo = false;;
+    if (testGo) {
+        macComet.moveUp()
+    } else  {
+        macComet.moveDown()
+    }
+    
+    macComet.show();
 	sun.show();
 	mercury.show();
 	mercury.rotateLeft();

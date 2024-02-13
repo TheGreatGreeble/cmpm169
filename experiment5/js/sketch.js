b3e2cfe16a68efac342db547adcab7973a6d2d07
GreebleCultist <evanlake583@gmail.com>
2024-02-12 16:46:21 -0800

    made the star particle move towards a given moving point

--- experiment5/js/sketch.js
+++ experiment5/js/sketch.js
@@ -51,28 +51,55 @@ class solarSystem{
 }
 
 class starParticle{
-    constructor(x,y,z, speed, r, img){
-		this.x=x
-		this.y=y
-		this.z=z
-        this.speed = speed;
+    constructor(pos, vel, des, speed, r, img){
+		this.pos = pos; // current position vector
+        this.vel = vel; // current velocity
+		this.des = des; // desired position
+		this.speed = speed;
         this.r = r;
 		this.img=img
+		this.done = false;
+		this.watch = 2000;
 	}
 	show(){
         push();
         normalMaterial();
-        translate(this.x,this.y,this.z);
+		if (this.done == false) {
+			if (this.watch > 100) {
+				this.updateVelocity(0.2);
+				this.watch = 0;
+			}
+			this.watch += deltaTime;
+			this.addVelocity();
+		}
+		translate(this.pos.x,this.pos.y,this.pos.z);
+		this.checkPos();
         texture(this.img);
         sphere(this.r);
         pop();
 	}
-	moveUp() {
-        this.y -= this.speed;
+	checkPos() {
+		if ((p5.Vector.sub(this.pos,this.des)).mag() < 0.1) {
+			this.done = true;
+		} else {
+			this.done = false;
+		}
+	}
+	updateVelocity(perc) {
+		var temp = p5.Vector.sub(this.des, this.pos);
+		this.vel.lerp(temp, perc);
+		this.vel.add((p5.Vector.random3D()).mult(this.speed));
+		this.vel.normalize();
+		this.vel.mult(lerp(-10,this.pos.dist(this.des),perc));
+		this.vel.limit(p5.Vector.dist(this.des,this.pos))
+
+	}
+	addVelocity() {
+        this.pos.add(this.vel);
     }
-    moveDown() {
-        this.y += this.speed;
-    }
+	changeDes(vec) {
+		this.des = vec;
+	}
 }
 // Here is how you might set up an OOP p5.js project
 // Note that p5.js looks for a file called sketch.js
@@ -98,6 +125,8 @@ function preload() {
 	
 }
 
+var starDest;
+var comets = [];
 // setup() function is called once when the program starts
 function setup() {
     // place our canvas, making it fit our container
@@ -110,9 +139,18 @@ function setup() {
         resizeCanvas(canvasContainer.width(), canvasContainer.height());
     });
 
+	angleMode(DEGREES);
     background(100);
-    macComet = new starParticle(0,0,0,2,30,macImage);
-	sun= new solarSystem(0,0,0,0,0,100,sunImage);
+	starDest = createVector(0, 150);
+	for (i = 0; i < 7; i++) {
+		comets[i] = new starParticle(
+			createVector(0,0,0),
+			createVector(50,-50,100),
+			createVector(-200,0,0),
+			5,15,macImage
+		);
+	}
+	sun= new solarSystem(0,0,0,0,0,1,sunImage);
 	mercury = new solarSystem(140,0,0,0,0,20,mercuryImage);
 	venus = new solarSystem(300,0,0,0,0,60,venusImage);
 	earth = new solarSystem(450,0,0,0,0,70,earthImage);
@@ -125,25 +163,42 @@ function setup() {
 }
     
 var testGo = false;
+var destY = 100;
+var yUP = true;
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
 	orbitControl(); 
-	background(100);
+	//background(100);
     push();
+	translate(0,150,80);
+	rotateX(90);
+	scale(10);
     normalMaterial();
     texture(bookTexture);
     model(book);
     pop();
-    if (macComet.y >= 200) testGo = true;
-    else if (macComet.y < -200) testGo = false;;
-    if (testGo) {
-        macComet.moveUp()
-    } else  {
-        macComet.moveDown()
-    }
+	space.show();
     
-    macComet.show();
+	//starDest.add(random(-50,50),random(-50,50),random(-50,50))
+	//starDest.limit(100);
+	starDest.rotate(3);
+	if (destY < -100) {
+		yUP = false;
+	} else if (destY > 100){
+		yUP = true;
+	}
+	if (yUP) destY -= 2;
+	if (!yUP) destY += 2;
+	for (i = 0; i < 7; i++) {
+		comets[i].changeDes(createVector(starDest.x,destY,starDest.y));
+		comets[i].show();
+	}
+	sun.x = starDest.x;
+	sun.y = destY;
+	sun.z = starDest.y;
 	sun.show();
+    
+	/*
 	mercury.show();
 	mercury.rotateLeft();
 	mercury.orbitMercury();
@@ -168,7 +223,7 @@ function draw() {
 	neptune.show();
 	neptune.rotateLeft();
 	neptune.orbitNeptune();
-	space.show();
+	*/
 }
 
   

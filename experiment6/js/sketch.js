430391b73fd06f6a1462606703c9a2b35ef10a5f
GreebleCultist <evanlake583@gmail.com>
2024-02-20 18:01:39 -0800

    added start/stop buttons

--- experiment6/js/sketch.js
+++ experiment6/js/sketch.js
@@ -14,6 +14,7 @@ var xSeed;
 var wSeed;
 var left =0;
 let myFont;
+var isGoTime = false;
 function preload() {
   //myFont = loadFont('MonumentExtended-Regular.otf');
 }
@@ -40,12 +41,14 @@ function setup() {
     HateButton.mousePressed(spawnHate);
 	HungyButton = select("#Hungy");
     HungyButton.mousePressed(spawnHungy);
+	goTimeButton = select("#GoTime");
+    goTimeButton.mousePressed(goTime);
 	
-    pixelDensity(1);
+    pixelDensity(0.5);
 	img = createGraphics(width,height);
 	xSeed = round(0.6*width);
     wSeed = width-xSeed;
-	img.pixelDensity(1);
+	img.pixelDensity(0.5);
 	img.fill(0);
 	img.noStroke();
 	img.rect(0,0,width,height);
@@ -72,8 +75,15 @@ function spawnHungy() {
 	img.fill(20,255,20);
 	textToScreen("Hungy");
 }
+function goTime() {
+	
+	isGoTime = !isGoTime;
+	print("GOTIME" + isGoTime);
+	image(img,0,0);
+}
 
 function textToScreen(str) {
+	loadPixels();
 	img.pixels = pixels;
 	print("spawnText");
 	//pixelDensity(1);
@@ -85,68 +95,33 @@ function textToScreen(str) {
 	//text(str, width/2, width/2);
 	image(img,0,0);
 	//filter(THRESHOLD);
-	//updatePixels();
 }
 
-var rSpread = 0.05;
-var gSpread = 0.15;
-var bSpread = 0.05;
+var rSpread = 0.1;
+var gSpread = 0.3;
+var bSpread = 0.1;
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
 	loadPixels();
 	//print(pixels[0] + "/" + pixels[1] + "/" + pixels[2]);
-	for (var i = 0; i < 4*(width*height); i+=4) {
-		var r = pixels[i];
-		var g = pixels[i+1];
-		var b = pixels[i+2];
-		var a = pixels[i+3];
-		//print (r + "/" + g + "/" + b);
-		if (r > 0) {
-			//print("found Red");
-		}
-        if (r > 20) {
-			updatePix(r,g,b, i, a, rSpread);
-		}
-		if (g > 20) {
-			updatePix(r,g,b, i+1, a, gSpread);
-		}
-		if (b > 20) {
-			updatePix(r,g,b, i+2, a, bSpread);
-		}
+	if (isGoTime) {
+		for (var i = 0; i < 4*(width*height); i+=4) {
+			var r = pixels[i];
+			var g = pixels[i+1];
+			var b = pixels[i+2];
+			var a = pixels[i+3];
+			
+			if (r > 20 && r > g && r > b) {
+				updatePix(r,g,b, i, a, rSpread);
+			} else if (g > 20  && g > r && g > b) {
+				updatePix(r,g,b, i+1, a, gSpread);
+			} else if (b > 20  && b > r && b > g) {
+				updatePix(r,g,b, i+2, a, bSpread);
+			}
 
-    }
-	updatePixels();
-	/*
-    xOff=1;
-  	yOff=height-1;
-	var w4 = (width*4);
-  	while(xOff<(width)){
-		r = round(cos(theta)*random(0,xOff));
-		var strt = yOff*w4;
-		var end = strt+(r<<2);
-		for(var i = end-12; i>strt; i-=12){
-			//pixels[i+12]=pixels[i+5]=pixels[i+6]=pixels[i];
-		}//copy(0,yOff,r,1,  1,yOff,r,1); 
-		left = Math.max(0,xSeed-r);
-		remain = Math.max(0,(width-left));
-		strt = (yOff-1)*w4+(left*12)+w4;
-		end = strt+(remain*4);
-		for(var i = strt; i<end; i+=12){
-			//pixels[i-w4]=pixels[i+1-w4]=pixels[i+2-w4]=pixels[i];
 		}
-    	//copy(left,yOff, remain,1,    left,yOff-1, remain,1);
-  		strt = (yOff*w4)+((width-r)*12);
-		end = strt+(r*5);
-		for(var i = strt+12; i<end; i+=12){
-			//pixels[i-12]=pixels[i-3]=pixels[i-2]=pixels[i];
-			//pixels[i-12+w4]=pixels[i-3+w4]=pixels[i-2+w4]=pixels[i+w4];
-		}//copy(width-r,yOff,r,2,   width-r-1,yOff,r,2);
-		xOff++;
-		yOff--;
-  	}
-	//updatePixels();
-	theta++;
-	*/
+	}
+	updatePixels();
 }
 
 function updatePix(r,g,b, i, a, spread) {
@@ -155,29 +130,29 @@ function updatePix(r,g,b, i, a, spread) {
 		case 0:
 			//print("up");
 			var upPix = i - (width*4);
-			if (upPix < 0) break;
-			pixels[upPix] += round(pixels[i] * spread*2);
+			if (upPix < 0) upPix = width*height*4 - upPix;
+			pixels[upPix] += pixels[i] * spread*2;
 			//pixels[a - width] = 255;
 			break;
 		case 1:
 			var rightPix = i + 4;
 			if (rightPix > (4*width*height)) break;
-			pixels[rightPix] += round(pixels[i] * spread*2);
+			pixels[rightPix] += pixels[i] * spread;
 			//pixels[a + 4] = 255;
 			break;
 		case 2:
 			var downPix = i + (width*4);
 			if (downPix > (4*width*height)) break;
-			pixels[downPix] += round(pixels[i] * spread*2);
+			pixels[downPix] += pixels[i] * spread;
 			//pixels[a + width] = 255;
 			break;
 		case 3:
 			var leftPix = i - 4;
-			if (leftPix < 0) break;
-			pixels[leftPix] += round(pixels[i] * spread*2);
+			if (leftPix < 0) leftPix = width*height*4 - leftPix;
+			pixels[leftPix] += pixels[i] * spread;
 			//pixels[a - 4] = 255;
 			break;
 		
 	}
-	pixels[i] *= (1 - spread);
+	pixels[i] -= pixels[i] * spread;
 }

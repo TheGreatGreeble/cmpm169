1c3232b1c0ff7cef8094f8a9709995547c1858d0
TheGreatGreeble <73497579+TheGreatGreeble@users.noreply.github.com>
2024-02-25 02:07:46 -0800

    got basic image quary working

--- experiment7/js/sketch.js
+++ experiment7/js/sketch.js
@@ -4,26 +4,25 @@
 
 // Here is how you might set up an OOP p5.js project
 // Note that p5.js looks for a file called sketch.js
-
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
 // Globals
 let myInstance;
 let canvasContainer;
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
+$.ajax({ 
+    url: "https://api.nasa.gov/planetary/apod", // API endpoint
+    data: { api_key: "DEMO_KEY"},      // Any data to send
+    //url: "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos", // API endpoint
+    //data: { earth_date:2015-6-3, api_key: "DEMO_KEY"},      // Any data to send
+    type: "GET",           // POST or GET request
+    dataType : "json", // expected data type
+    success: function(result){
+        //$("body").html("<h1>"+result.title); 
+        $("#canvas-container").html("<img src="+result.url+">");
+        //$("body").append("<p>"+result.explanation);
+    },
+    error: function(xhr,status,error) {
+        console.log("Error:",xhr,status,error); }
+})
 
 // setup() function is called once when the program starts
 function setup() {
@@ -36,32 +35,8 @@ function setup() {
         console.log("Resizing...");
         resizeCanvas(canvasContainer.width(), canvasContainer.height());
     });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
-
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
 }
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
-
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
-}
-
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
 }
